4074497733d1b8c27b9fab1bce4aaa19fcfa981b
Mike Garringer <michaelleelyngarringer@gmail.com>
2019-05-30 18:17:57 -0500

    added ability to look up item by id and have the console out the price, quantity and product name for use in a future version that will allow user to add item to cart as well as basic version of the cart commands)

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,7 +18,7 @@ function start() {
     .prompt({
       name: "prompt",
       type: "list",
-      message: "Would you like to [BUY] an auction or [VIEW] on an auction?",
+      message: "Would you like to [BUY] or [VIEW]?",
       choices: ["BUY", "VIEW", "EXIT"]
     })
     .then(function (answer) {
@@ -36,19 +36,41 @@ function start() {
 
 
 function buy() {
-  console.log("Feature coming soon!")
-  start();
+  inquirer
+    .prompt({
+      name: "buyWhat",
+      type: "input",
+      message: "Please insert the item ID of the item you wish to purchase? (If you don't know, you can always VIEW)"
+    }).then(function (answer) {
+      var query = "SELECT product_name, price, stock_quantity FROM products WHERE ?";
+      connection.query(query, { item_id: answer.buyWhat }, function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+          console.log("Product: " + res[i].product_name + " || Price: " + res[i].price + " || Number in Stock: " + res[i].stock_quantity);
+          again();
+        }
+      });
+    })
 }
+
 function view() {
   console.log("================")
   connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
     else {
       for (var i = 0; i < res.length; i++) {
-        console.log("ID:"+ res[i].item_id +" || Product Name: " + res[i].product_name + " || Department: " + res[i].department_name + " || Price: " + res[i].price + " || Number in stock: " + res[i].stock_quantity);     
+        console.log(
+          `
+        --------------------------------------------  
+        || ID: ${res[i].item_id} 
+        || Product Name: ${res[i].product_name} 
+        || Department:  ${res[i].department_name}  
+        || Price: ${res[i].price} 
+        || Number in stock: ${res[i].stock_quantity}
+        --------------------------------------------
+        `);
       }
-console.log("================")
-start();
+
+      start();
 
 
     }
@@ -56,4 +78,28 @@ start();
   )
 }
 
-
+function again() {
+  inquirer
+    .prompt({
+      name: "addToCart",
+      type: "list",
+      message: "Would you like to add this item to your cart?",
+      choices: ["Yes", "No", "View", "Exit"]
+    })
+    .then(function (answer) {
+      switch (answer.addToCart) {
+        case "Yes":
+         console.log("Function coming soon!")
+          break;
+        case "No":
+          buy();
+          break;
+        case "View":
+          view()
+          break;
+        case "Exit":
+          start();
+          break;
+      }
+    })
+}
