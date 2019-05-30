adf42f70bb8ae05226b0597744d58b1b63da10e0
Mike Garringer <michaelleelyngarringer@gmail.com>
2019-05-29 20:09:06 -0500

    View function added: This function allows the user to see what products are available, what department it is in, the product ID, Price and number still in stock

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
   password: "",
   database: "bamazon"
 });
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   start();
 });
@@ -21,26 +21,39 @@ function start() {
       message: "Would you like to [BUY] an auction or [VIEW] on an auction?",
       choices: ["BUY", "VIEW", "EXIT"]
     })
-    .then(function(answer) {
+    .then(function (answer) {
       // based on their answer, either call the bid or the post functions
       if (answer.prompt === "BUY") {
         buy();
       }
-      else if(answer.prompt === "VIEW") {
+      else if (answer.prompt === "VIEW") {
         view();
-      } else{
+      } else {
         connection.end();
       }
     });
 }
 
 
-function buy(){
+function buy() {
   console.log("Feature coming soon!")
   start();
 }
-function view(){
-  console.log("Feature coming soon!")
-  start();
+function view() {
+  console.log("================")
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    else {
+      for (var i = 0; i < res.length; i++) {
+        console.log("ID:"+ res[i].item_id +" || Product Name: " + res[i].product_name + " || Department: " + res[i].department_name + " || Price: " + res[i].price + " || Number in stock: " + res[i].stock_quantity);     
+      }
+console.log("================")
+start();
+
+
+    }
+  }
+  )
 }
+
 
